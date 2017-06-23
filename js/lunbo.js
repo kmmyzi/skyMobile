dd02c0a54bcc0461c03b8879e2ca88503a41d45d
guojiawei <18734869369@163.com>
2017-06-23 18:24:46 +0800

    初步完成，待测试。

---
+++ js/lunbo.js
@@ -0,0 +1,34 @@
+ $(document).ready(function(){
+
+          $('.slider2').bxSlider({
+
+            slideWidth: 300, 
+
+			auto: true,
+
+  			autoControls: true,
+
+            minSlides: 2,
+
+            maxSlides: 2,
+
+            slideMargin: 10
+
+          });
+$('.slider3').bxSlider({
+
+            slideWidth: 300, 
+
+			auto: true,
+
+  			autoControls: true,
+
+            minSlides: 2,
+
+            maxSlides: 2,
+
+            slideMargin: 10
+
+          });
+
+        });
